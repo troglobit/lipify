42f53ccac874846afac9741ff3b7de48a4d2e003
Joachim Nilsson <troglobit@gmail.com>
2016-05-01 10:20:30 +0200

    Update copyright years
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- ipify.c
+++ ipify.c
@@ -1,6 +1,6 @@
 /* Simple C library for querying your IP address using http://ipify.org
  *
- * Copyright (c) 2015  Joachim Nilsson <troglobit@gmail.com>
+ * Copyright (c) 2015-2016  Joachim Nilsson <troglobit@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
