5cd61535ce5b858ea8bc6ce3216a8d94483b20ca
Joachim Nilsson <troglobit@gmail.com>
2019-02-02 18:44:00 +0100

    Add basic option handling
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 /* The contents of this file is in the public domain. */
 #include <ipify.h>
+#include <string.h>
+#include <sys/socket.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	int family = AF_UNSPEC;
 	char addr[256];
-	int sd;
+	int i, sd;
 
-	sd = ipify_connect();
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-h")) {
+			printf("ipify [-46h]\n");
+			return 0;
+		}
+		if (!strcmp(argv[i], "-4")) {
+			family = AF_INET;
+			continue;
+		}
+		if (!strcmp(argv[i], "-6")) {
+			family = AF_INET6;
+			continue;
+		}
+		
+		printf("Invalid option: '%s'\n", argv[i]);
+		return 1;
+	}
+
+	sd = ipify_connect1(family);
 	if (sd < 0)
 		return 1;
 
